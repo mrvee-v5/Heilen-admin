482a6f2241a5ce048ea3ab317d0cc9e6e74624ad
mrvee <ebukavincent10@gmail.com>
2025-12-23 08:29:11 +0100

    enviroment

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 export const BACKEDN_API_BASE_URL =
-  process.env.APP_ENVIROMENT === 'production'
+  process.env.APP_ENVIROMENT==='production'
     ? 'https://api.heilen.io'
     : 'https://dev.heilen.io';
